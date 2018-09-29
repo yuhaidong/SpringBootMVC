88e282934599cbedc519824174f2f181627e7fac
yuhaidong <ryang2007@163.com>
2018-09-29 15:56:27 +0800

    数据库SQL

---
+++ db/db.sql
@@ -0,0 +1,52 @@
+-- MySQL dump 10.13  Distrib 5.6.40, for macos10.13 (x86_64)
+--
+-- Host: localhost    Database: sb
+-- ------------------------------------------------------
+-- Server version	5.6.40
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `sb_user`
+--
+
+DROP TABLE IF EXISTS `sb_user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `sb_user` (
+  `id` varchar(100) NOT NULL COMMENT '主键id',
+  `login_name` varchar(100) DEFAULT NULL COMMENT '用户登录名称',
+  `password` varchar(100) DEFAULT NULL COMMENT '登录密码',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='使用SpringBoot搭建的一个简单系统的登陆用户';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sb_user`
+--
+
+LOCK TABLES `sb_user` WRITE;
+/*!40000 ALTER TABLE `sb_user` DISABLE KEYS */;
+INSERT INTO `sb_user` VALUES ('1','j','c4ca4238a0b923820dcc509a6f75849b');
+/*!40000 ALTER TABLE `sb_user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-09-29 15:54:17
